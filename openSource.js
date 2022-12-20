1db2702f6db5371fe0b669e3608353d9fafd9ffc
thzero <thzero@users.noreply.github.com>
2022-12-20 07:40:36 -0600

    updates for opensource

---
+++ openSource.js
@@ -0,0 +1,58 @@
+export default () => {
+    return [
+        {
+            category: 'client',
+            name: '@thzero/library_client',
+            url: 'https://github.com/thzero/library_client',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/thzero/library_client/blob/master/license.md'
+        },
+        {
+            category: 'client',
+            name: '@thzero/library_client_vue3',
+            url: 'https://github.com/thzero/library_client_vue3',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/thzero/library_client_vue3/blob/master/license.md'
+        },
+        {
+            category: 'client',
+            name: '@thzero/library_common',
+            url: 'https://github.com/thzero/library_common',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/thzero/library_common/blob/master/license.md'
+        },
+        {
+            category: 'client',
+            name: 'vee-validate',
+            url: 'https://github.com/logaretm/vee-validate',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/logaretm/vee-validate/blob/master/LICENSE'
+        },
+        {
+            category: 'client',
+            name: 'vue',
+            url: 'https://github.com/vuejs/vue',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/vuejs/vue/blob/dev/LICENSE'
+        },
+        {
+            category: 'client',
+            name: 'vue3-async-computed',
+            url: 'https://github.com/mainclass/vue3-async-computed'
+        },
+        {
+            category: 'client',
+            name: 'vue-i18n',
+            url: 'https://github.com/kazupon/vue-i18n',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/kazupon/vue-i18n/blob/dev/LICENSE'
+        },
+        {
+            category: 'client',
+            name: 'vue-router',
+            url: 'https://github.com/vuejs/vue-router',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/vuejs/vue-router/blob/dev/LICENSE'
+        }
+    ];
+}
