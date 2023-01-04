338a5e7de6a4130607a98de895df35a6b5f03786
thzero <thzero@users.noreply.github.com>
2023-01-03 23:34:59 -0600

    updates

---
+++ components/form/VSwitchWithValidation.vue
@@ -0,0 +1,88 @@
+<template>
+	<v-switch
+		v-model="innerValue"
+		:hide-details="hideDetails"
+		:readonly="readonly"
+		:disabled="disabled"
+		:hint="$attrs.hint"
+		:label="$attrs.label"
+		density="compact"
+		:color="color"
+		@update:modelValue="change"
+	>
+		<template v-slot:details>
+			<div
+				v-for="error of errorsI"
+				:key="error.$uid"
+			>
+				<strong>{{ error.$message }}</strong>
+				<small> on </small>
+				<strong>{{ error.$property }}</strong>
+			</div>
+		</template>
+	</v-switch>
+</template>
+
+<script>
+import { useBaseControlEditComponent } from '@/library_vue/components/baseControlEdit';
+import { useBaseControlEditProps } from '@/library_vue/components/baseControlEditProps';
+
+export default {
+	name: 'VtSwitchWithValidation',
+	props: {
+		...useBaseControlEditProps,
+		color: {
+			type: String,
+			default: 'primary'
+		}
+	},
+	setup (props, context) {
+		const {
+			correlationId,
+			error,
+			hasFailed,
+			hasSucceeded,
+			initialize,
+			logger,
+			noBreakingSpaces,
+			notImplementedError,
+			success,
+			isSaving,
+			serverErrors,
+			setErrors,
+			convertValue,
+			errorI,
+			errorsI,
+			hideDetails,
+			innerValue,
+			innerValueUpdate,
+			initValue
+		} = useBaseControlEditComponent(props, context);
+		
+		return {
+			correlationId,
+			error,
+			hasFailed,
+			hasSucceeded,
+			initialize,
+			logger,
+			noBreakingSpaces,
+			notImplementedError,
+			success,
+			isSaving,
+			serverErrors,
+			setErrors,
+			convertValue,
+			errorI,
+			errorsI,
+			hideDetails,
+			innerValue,
+			initValue,
+			innerValueUpdate
+		};
+	}
+};
+</script>
+
+<style scoped>
+</style>
--- utility/index.js
+++ utility/index.js
@@ -1,3 +1,5 @@
+import { unref } from 'vue';
+
 import Constants from '../constants';
 import LibraryConstants from '@thzero/library_client/constants';
 import LibraryCommonConstants from '@thzero/library_common/constants';
@@ -73,11 +75,19 @@ class Utility {
 		return true;
 	}
 
+	static fullscreen(vuetify) {
+		return Utility.fullscreenDisplay(vuetify.display);
+	}
+
+	static fullscreenDisplay(display) {
+		return unref(unref(display).xs);
+	}
+
 	static handleError(object, serverErrors, response) {
 		if (!object || !serverErrors || !response)
 			return;
 
-		const errors = {};
+		let errors = {};
 		let error;
 		for (const field in response.errors) {
 			error = Utility.applyError(response.errors[field]);
@@ -92,6 +102,11 @@ class Utility {
 		}
 
 		object.setErrors(errors);
+		// object.validation().applyResult({
+		//     errors: [ GlobalUtility.$trans.t(`${Constants.ErrorCodes.Suffix}.${messageCode}`, messageParams) ],
+		//     valid: false,
+		//     failedRules: {} // should be empty since this is a manual error.
+		//   })
 	}
 
 	static invalid(next) {
@@ -102,9 +117,90 @@ class Utility {
 
 		GlobalUtility.$navRouter.push('/');
 	}
+
+	static overlayImageWidth() {
+		let width = (window.innerWidth > window.innerHeight ? window.innerHeight : window.innerWidth);
+		if (width > 512)
+			return '512px';
+
+		return width *.75 + 'px';
+	}
+
+	static overlayProgressSize() {
+		return (window.innerWidth > window.innerHeight ? window.innerHeight : window.innerWidth) * .25;
+	}
+
+	static randomKeyGen() {
+		const high = 100000000000;
+		const low = 0;
+		return Math.floor(Math.random() * (high - low) + low);
+	}
+
+	static selectBlank(array, prompt) {
+		if (!array)
+			return array;
+
+		prompt = prompt ? '<' + prompt + '>' : '';
+
+		const temp = array.slice(0);
+		temp.unshift({ id: null, name: prompt });
+		return temp;
+	}
 	
+	static selectOptions(options, trans, prefix, funcId, funcName, funcValue) {
+		if (!options || !trans || !Array.isArray(options))
+			return [];
+
+		prefix = (prefix && prefix !== '') ? prefix + '.' : '';
+
+		const output = options.map(l => {
+			let id = l;
+			if (funcId)
+				id = funcId(l);
+
+			let nameLookup = id;
+			if (funcName)
+				nameLookup = funcName(l);
+			nameLookup = prefix + nameLookup;
+			let name = trans(nameLookup);
+			if (String.isNullOrEmpty(name) || name === nameLookup)
+				name = trans(nameLookup + '.title');
+
+			let value = l;
+			if (funcValue)
+				value = funcValue(l);
+
+			return {
+				id: id,
+				name: name,
+				value: value
+			};
+		});
+		return output;
+	}
+
 	static settings() {
 		return GlobalUtility.$injector.getService(LibraryConstants.InjectorKeys.SERVICE_SETTINGS);
+	}
+
+	static updateArrayById(array, object) {
+		if (!object)
+			return;
+
+		// let index = array.findIndex(function(element) {
+		// 	return element && element.id == object.id
+		// })
+		// if (index === -1)
+		// 	array.push(object)
+		// else
+		// 	array[index] = object
+
+		// return array
+		let result = [
+			...array.filter(element => element.id !== object.id),
+			object
+		];
+		return result;
 	}
 }
 
